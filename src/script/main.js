831667f69cfab4e614cc6f4dbca118f7353f1630
caiocroccia22 <caiocroccia22@gmail.com>
2025-03-11 22:04:06 -0300

    feat: implement counter with setInterval

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,21 @@
-console.log('oi');
+const contador = document.getElementById('count');
+
+var date = new Date('Apr 10 2025 22:00:00 GMT-0300').getTime();
+// console.log(date);
+
+
+
+setInterval (function() {
+    
+    var now = new Date().getTime();
+    // console.log(now);
+
+    var distance = date - now;
+    // console.log(distance);
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000  *  60 * 60));
+
+    contador.innerHTML = days + 'Dias'+ ' ' + hours + 'Horas';
+}, 1000);
+
